10adcf17044955411d8f3eef3a30d245b2ca3246
Markus Muinonen <markus.muinonen@gmail.com>
2020-03-27 10:46:48 +0200

    1.6 done.

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,15 +1,34 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = ({text}) => <h1>{text}</h1>
+const Button = ({handler, text}) => <button onClick={handler}>{text}</button>
+const Stats = ({good, neutral, bad}) => {
+  return(
+    <div>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+    </div>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleClick = (state, setState) => () => setState(state + 1)
+  
   return (
     <div>
-      code here
+      <Header text="give feedback"/>
+      <Button handler={handleClick(good, setGood)} text="good" />
+      <Button handler={handleClick(neutral, setNeutral)} text="neutral" />
+      <Button handler={handleClick(bad, setBad)} text="bad" />
+      <Header text="statistics"/>
+      <Stats good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
