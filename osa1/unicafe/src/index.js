7632a9c735bed5d15f49073847630eb982fd581b
Markus Muinonen <markus.muinonen@gmail.com>
2020-03-27 12:32:03 +0200

    1.11 done.

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -14,17 +14,21 @@ const Statistics = ({good, neutral, bad}) => {
   }
   return(
     <div>
-      <StatisticLine text="good" value={good}/>
-      <StatisticLine text="neutral" value={neutral}/>
-      <StatisticLine text="bad" value={bad}/>
-      <StatisticLine text="all" value={sum}/>
-      <StatisticLine text="average" value={average}/>
-      <StatisticLine text="positive" value={average + " %"}/>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good}/>
+        <StatisticLine text="neutral" value={neutral}/>
+        <StatisticLine text="bad" value={bad}/>
+        <StatisticLine text="all" value={sum}/>
+        <StatisticLine text="average" value={average}/>
+        <StatisticLine text="positive" value={positivePercent + " %"}/>
+      </tbody>
+    </table>
     </div>
   )
 }
 
-const StatisticLine = ({text, value}) => <div>{text} {value}</div>
+const StatisticLine = ({text, value}) => <tr><td>{text}</td><td>{value}</td></tr>
 
 const App = () => {
   // tallenna napit omaan tilaansa
