3dd2c16a7044a004d9e59715302dd5606c517e6a
Markus Muinonen <markus.muinonen@gmail.com>
2020-03-29 12:50:40 +0300

    2.6 Fixed a mistake, now App is a component.

--- osa2/puhelinluettelo/src/index.js
+++ osa2/puhelinluettelo/src/index.js
@@ -1,27 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
 
-const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      ...
-    </div>
-  )
-
-}
-
-export default App
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
