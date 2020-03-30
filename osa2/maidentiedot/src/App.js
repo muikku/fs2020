d7b00b27df00efb1ccdca066b3a77ff18d39c90a
Markus Muinonen <markus.muinonen@gmail.com>
2020-03-30 14:42:54 +0300

    2.14 can use weatherstack api now. Refactoring needed.

--- osa2/maidentiedot/src/App.js
+++ osa2/maidentiedot/src/App.js
@@ -6,13 +6,28 @@ import axios from 'axios'
 
 function App() {
   const [ countryData, setCountryData ] = useState([])
+  const [ weatherData, setWeatherData ] = useState([])
   const [ filter, setFilter ] = useState('')
-
+  const api_key = process.env.REACT_APP_API_KEY
   useEffect(() => {
     axios
     .get('https://restcountries.eu/rest/v2/all')
     .then(res => {setCountryData(res.data)})
   }, [])
+
+  
+  const getWeather = (text) => {
+    const params = {
+      access_key: api_key,
+      query: text
+    }
+    axios
+      .get('http://api.weatherstack.com/current',{params})
+      .then(res => {setWeatherData(res.data)})
+  }
+  useEffect(() =>{
+    getWeather('Oslo')
+  },[])
 
   const onFilterChange = (event) => setFilter(event.target.value)
   const onButtonClick = (data) => setFilter(data)
@@ -25,7 +40,6 @@ function App() {
         filter={filter}
         onClick={onButtonClick}
       />
-      
     </div>
   )
 }
