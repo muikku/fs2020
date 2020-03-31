3f621f52a3da6bd1fb944d02fe5bf3168f8fcff6
Markus Muinonen <markus.muinonen@gmail.com>
2020-03-31 12:01:18 +0300

    2.14 done.

--- osa2/maidentiedot/src/App.js
+++ osa2/maidentiedot/src/App.js
@@ -3,24 +3,43 @@ import Filter from './components/Filter'
 import DisplayCountries from './components/DisplayCountries'
 import axios from 'axios'
 import DisplayCountry from './components/DisplayCountry';
-
-
+import weatherService from './services/weather'
 
 function App() {
   const [ countryData, setCountryData ] = useState([])
   const [ filter, setFilter ] = useState('')
-  
-  const scope = Filter.filteredData(countryData, filter)
-  const single = () => scope.length === 1 ? scope[0] : null
+  const [ weather, setWeather ] = useState(null)
+  const [ gotWeather, setWeatherGotten ] = useState(false)
 
   useEffect(() => {
     axios
     .get('https://restcountries.eu/rest/v2/all')
     .then(res => {setCountryData(res.data)})
   }, [])
+  
+  const scope = Filter.filteredData(countryData, filter)
+  const single = () => scope.length === 1 ? scope[0] : null
+  
+  const getWeather = (capital) => {
+    weatherService
+    .getCurrent(capital)
+    .then(res => {
+      setWeather(res.data)
+    })
+  }
 
   const onFilterChange = (event) => setFilter(event.target.value)
   const onButtonClick = (data) => setFilter(data)
+
+  if(!gotWeather && single()){
+    getWeather(single().capital)
+    setWeatherGotten(true)
+  }
+
+  if(!single() && gotWeather){
+    setWeatherGotten(false)
+  }
+  
 
   return (
     <div>
@@ -28,7 +47,7 @@ function App() {
       { 
         single() 
         ?
-        <DisplayCountry country={scope[0]}/>
+        <DisplayCountry country={scope[0]} weather={weather}/>
         :
         <DisplayCountries countryData={scope} onClick={onButtonClick}/>
       }
--- osa2/maidentiedot/src/components/DisplayCountry.js
+++ osa2/maidentiedot/src/components/DisplayCountry.js
@@ -1,27 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React from 'react'
 
-const DisplayCountry = ({country}) => {
-
-    const api_key = process.env.REACT_APP_API_KEY
-    const [ weather, setWeatherData ] = useState(null)
-
-    useEffect(() => {
-        const params = {
-            access_key: api_key,
-            query: country.capital
-        }
-        axios
-        .get('http://api.weatherstack.com/current', {params})
-        .then(res => {setWeatherData(res.data)})
-    }, [])
-
-    if(!weather){
-        return (
-        <div>Loading data...</div>
-        )
-    }
-
+const DisplayCountry = ({country, weather}) => {
     return (
     <div>
         <h1>{country.name}</h1>
@@ -32,10 +11,18 @@ const DisplayCountry = ({country}) => {
             {country.languages.map(e => <li key={e.name}>{e.name}</li>)}
         </ul>
         <img width={100} hight={100} src={country.flag} alt={`Flag of ${country.name}`}/>
-        <h2>Weather in {weather.location.name}</h2>
-        <div><strong>temperature:</strong> {weather.current.temperature} Celcius</div>
-        <img width={40} height={40} src={weather.current.weather_icons[0]} alt={weather.current.weather_descriptions[0]}/>
-        <div><strong>wind:</strong> {weather.current.wind_speed} mph direction {weather.current.wind_dir}</div>
+        {
+            !weather ? 
+            <div>Loading data...</div> 
+            :
+            <div>
+                <h2>Weather in {weather.location.name}</h2>
+                <div><strong>temperature:</strong> {weather.current.temperature} Celcius</div>
+                <img width={40} height={40} src={weather.current.weather_icons[0]} alt={weather.current.weather_descriptions[0]}/>
+                <div><strong>wind:</strong> {weather.current.wind_speed} mph direction {weather.current.wind_dir}</div>
+            </div>
+        }
+        
     </div>
     )
 }
