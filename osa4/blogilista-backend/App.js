9ddee4012af834026a06148391781e1c5725049f
Markus Muinonen <markus.muinonen@gmail.com>
2020-04-06 13:11:58 +0300

    4.1 and 4.2 done. Started using middleware and logger.

--- osa4/blogilista-backend/App.js
+++ osa4/blogilista-backend/App.js
@@ -4,14 +4,21 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 const blogRouter = require('./controllers/blogs')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
+  .then(() => logger.info('connected to MongoDB'))
+  .catch(error => logger.error('error connection to MongoDB:', error.message))
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
