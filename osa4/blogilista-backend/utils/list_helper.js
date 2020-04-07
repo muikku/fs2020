5fa2080b3d9d65184670ebb58bb0d865a2fcb2da
Markus Muinonen <markus.muinonen@gmail.com>
2020-04-07 14:12:08 +0300

    4.4 done. More unit tests.

--- osa4/blogilista-backend/utils/list_helper.js
+++ osa4/blogilista-backend/utils/list_helper.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-unused-vars */
 const dummy = (blogs) => 1
 
-
+const totalLikes = (blogs) => blogs.reduce((acc, curr) => acc += curr.likes, 0)
 
 
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
