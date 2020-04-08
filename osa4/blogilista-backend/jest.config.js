aa8d36b3d620f4a4d65769bb3c3a54c8d8447199
Markus Muinonen <markus.muinonen@gmail.com>
2020-04-08 20:19:19 +0300

    4.8 tests ready.

--- osa4/blogilista-backend/jest.config.js
+++ osa4/blogilista-backend/jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-	testEnviroment: 'node',
-};
+  testEnvironment: 'node'
+}
--- osa4/blogilista-backend/tests/blog_api.test.js
+++ osa4/blogilista-backend/tests/blog_api.test.js
@@ -1,15 +1,28 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../App')
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
 
 const api = supertest(app)
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
 })
+
+describe('HTTP GET BLOGS', () => {
+  test('blogs are returned as json and there is correct amount', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+})
+
+
 
 afterAll(() => {
   mongoose.connection.close()
