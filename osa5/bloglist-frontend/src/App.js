2ae221a7af9069508471b97bfebb3b7bba57a652
Markus Muinonen <markus.muinonen@gmail.com>
2020-04-13 13:21:43 +0300

    Made login form and service.

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Login from './components/Login'
 import blogService from './services/blogs'
+import loginService from './services/login'
+
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,8 +17,29 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try{
+      const user = await loginService.login({
+        username, password
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log(exception)
+    }
+  }
+
   return (
     <div>
+      <Login 
+      handleSubmit={handleLogin} 
+      username={username} 
+      password={password} 
+      handleUsernameChange={({target}) => setUsername(target.value)}
+      handlePasswordChange={({target}) => setPassword(target.value)}
+      />
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
