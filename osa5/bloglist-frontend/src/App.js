52c6833f43dab6c09617f4f71218164d50410f1e
Markus Muinonen <markus.muinonen@gmail.com>
2020-04-15 11:17:15 +0300

    5.9 done. Bloglist is sorted by likes.

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -103,7 +103,7 @@ const App = () => {
               />
             </Togglable>
             <div>
-              {blogs.map((blog) =>
+              {blogs.sort((a, b) => b.likes - a.likes).map((blog) =>
                 <Blog key={blog.id} blog={blog} handleLike={handleLiking} />
               )}
             </div>
