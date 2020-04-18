a890c548b1c8a4f92083ed33678f42e1bc9cf2bf
Markus Muinonen <markus.muinonen@gmail.com>
2020-04-18 20:58:59 +0300

    Deploying to heroku. For exercise.

--- osa4/bloglist-backend/App.js
+++ osa4/bloglist-backend/App.js
@@ -16,6 +16,7 @@ mongoose.connect(config.MONGODB_URI, { useCreateIndex: true, useNewUrlParser: tr
   .catch(error => logger.error('error connection to MongoDB:', error.message))
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
---
+++ osa4/bloglist-backend/build/precache-manifest.ba61c8fd91744ef35804349c79557f22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82010e298ee20607e5a5a2e318a2ba8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b440336bdd06c316e60a",
+    "url": "/static/js/2.87d6caff.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.87d6caff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13291804739e5a696c44",
+    "url": "/static/js/main.d74b5712.chunk.js"
+  },
+  {
+    "revision": "7eb8d6388ecf90523c80",
+    "url": "/static/js/runtime-main.7dc31f0f.js"
+  }
+]);
