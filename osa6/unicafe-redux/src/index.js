60ecce48c5593ea3f521ec6306913e5003443898
Markus Muinonen <markus.muinonen@gmail.com>
2020-04-19 12:47:01 +0300

    Fixed reducer.

--- osa6/unicafe-redux/src/index.js
+++ osa6/unicafe-redux/src/index.js
@@ -6,21 +6,34 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
+  const increment = (string) => () => {
     store.dispatch({
-      type: 'GOOD'
+      type: string
     })
+  }
+
+  const getState = (string) => {
+    switch(string){
+      case 'GOOD':
+        return store.getState().good
+        case 'OK':
+        return store.getState().ok
+        case 'BAD':
+        return store.getState().bad
+        default:
+        return store.getState()
+    }
   }
 
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>neutral</button> 
-      <button>bad</button>
-      <button>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>neutral</div>
-      <div>bad</div>
+      <button onClick={increment('GOOD')}>good</button> 
+      <button onClick={increment('OK')}>neutral</button> 
+      <button onClick={increment('BAD')}>bad</button>
+      <button onClick={increment('ZERO')}>reset stats</button>
+      <div>good {getState('GOOD')}</div>
+      <div>neutral {getState('OK')}</div>
+      <div>bad {getState('BAD')}</div>
     </div>
   )
 }
