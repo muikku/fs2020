49ecd9d4a7ac874132bed615ebeaa55371c8c3ae
Markus Muinonen <markus.muinonen@gmail.com>
2020-04-20 13:14:20 +0300

    Added filterReducer and using given Filter component.

---
+++ osa6/redux-anecdotes/src/reducers/filterReducer.js
@@ -0,0 +1,20 @@
+const reducer = (state = '', action) => {
+    switch(action.type) {
+        case 'FILTER':
+            return action.data
+        default: 
+        return state
+    }
+}
+
+export const addFilter = (text) => {
+    return {
+        type: 'FILTER',
+        data: text
+    }
+}
+
+
+
+export default reducer
+
