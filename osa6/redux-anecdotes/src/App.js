f75cb9f04ce99bafe61a6959f95dddac4cfe0e2e
Markus Muinonen <markus.muinonen@gmail.com>
2020-04-20 14:33:42 +0300

    6.13 done. Anecdotes are initialized from json-server.

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import anecdoteService from './services/anecdotes'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 import Filter from './components/Filter'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService.getAll().then(dotes => 
+    dispatch(initializeAnecdotes(dotes))
+  )
+  })
+  
   return (
     <div>
       <h2>Anecdotes</h2>
--- osa6/redux-anecdotes/src/store.js
+++ osa6/redux-anecdotes/src/store.js
@@ -5,6 +5,7 @@ import anecdoteReducer from './reducers/anecdoteReducer'
 import notificationReducer from './reducers/notificationReducer'
 import filterReducer from './reducers/filterReducer'
 
+
 const reducer = combineReducers({
     anecdotes: anecdoteReducer,
     notification: notificationReducer,
