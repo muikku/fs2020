aef6664e60e587a67f28910a0622ccaefa4fd480
Markus Muinonen <markus.muinonen@gmail.com>
2020-04-28 13:41:58 +0300

    Made component BlogList. Blog remove is not showing.

--- osa7/bloglist-frontend/src/App.js
+++ osa7/bloglist-frontend/src/App.js
@@ -1,20 +1,18 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import Blog from './components/Blog'
+import BlogList from './components/BlogList'
 import BlogForm from './components/BlogForm'
 import LoginForm from './components/LoginForm'
 import Logout from './components/Logout'
 import Notification from './components/Notification'
 import Togglable from './components/Togglable'
-import { notify } from './reducers/notificationReducer'
-import { initializeBlogs, likeBlog, removeBlog } from './reducers/blogReducer'
+import { initializeBlogs } from './reducers/blogReducer'
 import { loginFromLocalStorage } from './reducers/loginReducer'
 
 
 
 const App = () => {
   const dispatch = useDispatch()
-  const blogs = useSelector(state => state.blogs)
   const user = useSelector(state => state.login)
 
 
@@ -28,26 +26,6 @@ const App = () => {
       dispatch(loginFromLocalStorage(loggedUserJSON))
     }
   },[dispatch])
-
-
-
-  const handleLiking = async (obj) => {
-    try{
-      dispatch(likeBlog(obj))
-      dispatch(notify(`${obj.title} + 1 like!`, 5))
-    } catch(error){
-      dispatch(notify(`there was error liking blog ${obj.title}`, 5))
-    }
-  }
-
-  const handleDeleting = async (obj) => {
-    try{
-      dispatch(removeBlog(obj.id))
-      dispatch(notify(`${obj.title} deleted!`, 5))
-    } catch(error){
-      dispatch(notify(`an error occured when deleting ${obj.title}`, 5))
-    }
-  }
 
   return (
     <div>
@@ -67,17 +45,7 @@ const App = () => {
             <Togglable buttonLabel='new blog'>
               <BlogForm />
             </Togglable>
-            <div id="blogs">
-              {blogs.sort((a, b) => b.likes - a.likes).map((blog) =>
-                <Blog
-                  key={blog.id}
-                  blog={blog}
-                  handleLike={handleLiking}
-                  handleDelete={handleDeleting}
-                  user={user}
-                />
-              )}
-            </div>
+            <BlogList />
           </>
         )}
     </div>
