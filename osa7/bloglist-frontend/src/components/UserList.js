3ef8e28d771e466749a1fbfcc63065789d9e4e83
Markus Muinonen <markus.muinonen@gmail.com>
2020-05-01 19:42:48 +0300

    Changed UserList layout.

--- osa7/bloglist-frontend/src/components/UserList.js
+++ osa7/bloglist-frontend/src/components/UserList.js
@@ -1,40 +1,41 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { TableContainer, Paper, Table, TableCell, TableHead, TableBody, TableRow, makeStyles, Container } from '@material-ui/core'
+import { TableContainer, Paper, Table, TableCell, TableHead, TableBody, TableRow, Grid, Typography } from '@material-ui/core'
 
 
 const UserList = () => {
   const users = useSelector(state => state.users)
-  const useStyles = makeStyles({
-    table: {
-      maxWidth: 600
-    }
-  })
-  const classes = useStyles()
   return(
-    <Container maxWidth="sm">
-      <TableContainer component={Paper}>
-        <Table className={classes.table} size="small" aria-label="a dense table">
-          <TableHead>
-            <TableRow>
-              <TableCell>user</TableCell>
-              <TableCell align="right">blogs created</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {users.map((user) => (
-              <TableRow key={user.id}>
-                <TableCell component="th" scope="row">
-                  <Link to={`/users/${user.id}`}>{user.username}</Link>
-                </TableCell>
-                <TableCell align="right" scope="row">{user.blogs.length}</TableCell>
+    <Grid lignItems="stretch" justify="center" container direction="column" spacing={3}>
+      <Grid alignItems="center" container justify="center" direction="column">
+        <Grid item>
+          <Typography variant="h2">Users</Typography>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <TableContainer component={Paper}>
+          <Table >
+            <TableHead>
+              <TableRow>
+                <TableCell>user</TableCell>
+                <TableCell align="right">blogs created</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Container>
+            </TableHead>
+            <TableBody>
+              {users.map((user) => (
+                <TableRow key={user.id}>
+                  <TableCell component="th" scope="row">
+                    <Link to={`/users/${user.id}`}><Typography>{user.username}</Typography></Link>
+                  </TableCell>
+                  <TableCell align="right" scope="row"><Typography>{user.blogs.length}</Typography></TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+    </Grid>
   )
 }
 
