09b34890813f6b725044a84cba33ca4dae0cd529
Markus Muinonen <markus.muinonen@gmail.com>
2020-05-26 21:01:33 +0300

    9.15 done, I would think. More interface/assert/typing.

--- osa9/course-info/src/components/Part.tsx
+++ osa9/course-info/src/components/Part.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
-import { CoursePart } from '../types';
+import { CoursePart, assertNever } from '../types';
 
 const Part: React.FC<{ coursePart: CoursePart }> 
-  = ({ coursePart }) => 
-<p> 
-  {coursePart.name} {coursePart.exerciseCount}
-</p>
+  = ({ coursePart }) => {
+    switch(coursePart.name){
+      case 'Deeper type usage':
+        return (
+          <div> 
+            {coursePart.name} {coursePart.exerciseCount} {coursePart.description} {coursePart.exerciseSubmissionLink}
+          </div>
+        )
+      case 'Fundamentals':
+        return (
+          <div> 
+            {coursePart.name} {coursePart.exerciseCount} {coursePart.description}
+          </div>
+        )
+      case 'Using props to pass data':
+        return (
+          <div> 
+            {coursePart.name} {coursePart.exerciseCount} {coursePart.groupProjectCount}
+          </div>
+        )
+      
+      case 'Typescript':
+        return (
+            <div> 
+            {coursePart.name} {coursePart.exerciseCount} {coursePart.description}
+          </div>
+        )
+        default:
+          return assertNever(coursePart);
+    }
+
+  }
 
 export default Part;
--- osa9/course-info/src/types.tsx
+++ osa9/course-info/src/types.tsx
@@ -1,4 +1,28 @@
-export interface CoursePart {
+interface CoursePartBase {
   name: string;
   exerciseCount: number;
 }
+
+interface CoursePartTwo extends CoursePartBase {
+  name: "Using props to pass data";
+  groupProjectCount: number;
+}
+
+interface CoursePartWithDescription extends CoursePartBase {
+  name: 'Fundamentals' | "Typescript";
+  description: string;
+}
+
+interface CoursePartDeep extends CoursePartBase {
+  name: "Deeper type usage";
+  description: string;
+  exerciseSubmissionLink: string;
+}
+
+export const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  )
+}
+
+export type CoursePart = CoursePartTwo | CoursePartWithDescription | CoursePartDeep;
